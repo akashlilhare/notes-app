ae67d51ff3a788b4c09754ad03c9af0683eb7622
Akash Lilhare <akashlilhare14@gmail.com>
2021-04-12 23:12:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+
+const app = function(){
+    return <div>
+<Header />
+</div>;
+}
+
+
+export default app;
